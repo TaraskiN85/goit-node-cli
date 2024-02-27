139e4300297ba3c226df7ebe3bcb3edd886eaa8a
TaraskiN85 <taras12304@i.ua>
2024-02-27 23:42:20 +0200

    accomplish hw-01

---
+++ contacts.js
@@ -0,0 +1,38 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { nanoid } from 'nanoid';
+
+const contactsPath = path.resolve('./db', 'contacts.json');
+const saveContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+export async function listContacts() {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
+}
+
+export async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+  return contact || null;
+}
+
+export async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [deletedContact] = contacts.splice(index, 1);
+  await saveContacts(contacts);
+
+  return deletedContact;
+}
+
+export async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { name, email, phone, id: nanoid() };
+  contacts.push(newContact);
+  await saveContacts(contacts);
+  return newContact;
+}
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import { program } from "commander";
+import { program } from 'commander';
+import * as contactsService from './contacts.js';
+
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
 
 program.parse();
 
@@ -13,24 +15,26 @@ const options = program.opts();
 // TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
-    case "list":
-      // ...
+    case 'list':
+      const allContacts = await contactsService.listContacts();
+      return console.log(allContacts);
+
+    case 'get':
+      const contact = await contactsService.getContactById(id);
+      return console.log(contact);
+
+    case 'add':
+      const addedContact = await contactsService.addContact(name, email, phone);
+      return console.log(addedContact);
       break;
 
-    case "get":
-      // ... id
-      break;
-
-    case "add":
-      // ... name email phone
-      break;
-
-    case "remove":
-      // ... id
+    case 'remove':
+      const removedContact = await contactsService.removeContact(id);
+      return console.log(removedContact);
       break;
 
     default:
-      console.warn("\x1B[31m Unknown action type!");
+      console.warn('\x1B[31m Unknown action type!');
   }
 }
 
